441c1edf1ad0e495ebb18133805012b120ebce72
albesaaa <albesaaa@AMBL.lan>
2023-12-06 23:20:41 -0500

    Ana Albesa

--- Montoya_FinalProject/KanoutmaPackage/Kanoutma.py
+++ Montoya_FinalProject/KanoutmaPackage/Kanoutma.py
@@ -14,8 +14,8 @@ from PIL import Image
 
 
 def display_photo(photo_path):
-   '''
-   This function displays the photo at the decrypted location and a quote from    the decrypted movie title.
-   '''
-   img = Image.open(photo_path)
-   img.show()
+    '''
+    This function displays the photo at the decrypted location and a quote from    the decrypted movie title.
+    '''
+    img = Image.open(photo_path)
+    img.show()
--- Montoya_FinalProject/mainPackage/main.py
+++ Montoya_FinalProject/mainPackage/main.py
@@ -8,32 +8,30 @@
 # Citations: N/A
 # Anything else that's relevant: N/A
 
-
 from barryaoPackage.barryao import *
-from kanoutmaPackage.kanoutma import *
-from albessaaaPackage.albessaaa import *
-
+from KanoutmaPackage.Kanoutma import *
+from albesaaaPackage.albesaaa import *
 
 # Montoya: Location Hint
 if __name__ == "__main__":
-   encrypted_location_data = ["30942", "42547", "19312", "65919", "8879", "36488", "2756", "45854", "18755", "28894", "8018", "2756", "24979", "42636"]
-   english_file_path = 'english-2.txt'
-   decrypted_location = decrypt_location(encrypted_location_data, english_file_path)
-   print(f"Decrypted Location: {decrypted_location}")
-
+    encrypted_location_data = ["30942", "42547", "19312", "65919", "8879", "36488", "2756", "45854", "18755", "28894", "8018", "2756", "24979", "42636"]
+    english_file_path = 'english-2.txt'
+    
+    decrypted_location = decrypt_location(encrypted_location_data, english_file_path)
+    print(f"Decrypted Location: {decrypted_location}")
 
 # Montoya: Movie Title
 from cryptography.fernet import Fernet
 
-
 if __name__ == "__main__":
-   encrypted_message_file = "TeamsAndEncryptedMessagesForDistribution.json"
-   decryption_key = 'LMV69IGGTp2Gyn4TI-DTuupf0VvugeC5API5dpeoiqM='
-   fernet_key = Fernet(decryption_key)
-   decrypted_movie = decrypt_movie_title(fernet_key, encrypted_message_file)
-   print(f"Movie Title:", decrypted_movie)
+    encrypted_message_file = "TeamsAndEncryptedMessagesForDistribution.json"
+    decryption_key = 'LMV69IGGTp2Gyn4TI-DTuupf0VvugeC5API5dpeoiqM='
+    fernet_key = Fernet(decryption_key)
+    
+    decrypted_movie = decrypt_movie_title(fernet_key, encrypted_message_file)
+    print(f"Movie Title:", decrypted_movie)
   
 # Photo at the Montoya location with a quote from the Montoya movie.
 if __name__ == "__main__":
-   photo_path = "Final Photo.jpg"
-   display_photo(photo_path)
+    photo_path = "Final Photo.jpg"
+    display_photo(photo_path)
